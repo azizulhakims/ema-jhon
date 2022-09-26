dcad2e9671ae76f28fd1bb5b8e500f255d3519bc
azizulhakims <it.azizulhakim@gmsil.com>
2022-09-26 17:46:18 +0600

    add cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} = props;
+//  console.log(cart);
+
+ let total = 0;
+ let shipping = 0;
+ for(const product of cart){
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+ }
+
+ const tax = total * 0.1;
+
+ const grandTotal = total + shipping + tax;
+
+//  const Grand 
+
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summery</h4>
             <p>Selected Items: {cart.length}</p>
+            <p>Total price: ${total} </p>
+            <p>Total SHipping: ${shipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <p> <b> Grand Total: ${grandTotal.toFixed(2)} </b></p>
         </div>
     );
 };
